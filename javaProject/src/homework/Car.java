de227b303b7e41220d4cd38b4e1598e0641c33bb
tjddn <tjddn@DESKTOP-OF7FEPR>
2021-03-20 17:06:44 +0900

    3/20

---
+++ javaProject/src/homework/Car.java
@@ -0,0 +1,5 @@
+package homework;
+
+public class Car {
+	Car myCar = new Car();
+}
--- javaProject/src/objects2/Singleton.java
+++ javaProject/src/objects2/Singleton.java
@@ -1,9 +1,18 @@
 package objects2;
 
+import homework.Car;
+
 public class Singleton {
-		private static Singleton singleton = new Singleton();
-		private Singleton() {}
-		public static Singleton getInstance() {
-			return singleton;
-		}
+	Car myCar = new Car();
+	
+	
+	
+	private static Singleton singleton = new Singleton();
+
+	private Singleton() {
+	}
+
+	public static Singleton getInstance() {
+		return singleton;
+	}
 }
